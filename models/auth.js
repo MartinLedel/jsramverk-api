d75ca9baa70ecacfe5ebcbf050dfed55d3e11b25
MartinLedel <martinledel@gmail.com>
2020-10-02 09:30:43 +0200

    Trying to fix travis and scrutinizer

--- models/auth.js
+++ models/auth.js
@@ -1,6 +1,10 @@
 const jwt = require('jsonwebtoken');
 
-let config = require('../config/config.json');
+try {
+    config = require('../config/config.json');
+} catch (error) {
+    console.error(error);
+}
 
 const jwtSecret = process.env.JWT_SECRET || config.secret;
 
